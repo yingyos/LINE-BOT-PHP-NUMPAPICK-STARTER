75e4ca1cb0f2ef6d207052b781e226e11773d7b3
yingyos <36525213+yingyos@users.noreply.github.com>
2018-06-05 20:33:46 +0700

    Update pub.php
    +       $APPID= "FireAlarmWangnumyen/"; //enter your appid
    +     $KEY = "XABTm9JpQMouxAf"; //enter your key
    +    $SECRET = "x0eSODMGLlelkfhD1ehYeHA66"; //enter your secret

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "FireAlarmWangnumyen/"; //enter your appid
+     $KEY = "XABTm9JpQMouxAf"; //enter your key
+    $SECRET = "x0eSODMGLlelkfhD1ehYeHA66"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
