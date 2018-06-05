281e59e2ddbbb9369b470bd8e50a211af4d6b1d3
yingyos <36525213+yingyos@users.noreply.github.com>
2018-06-05 23:16:53 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,8 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+       'to' => '9e70b004838089aea031a24e2cd49625',
+       
         'messages' => [$messages],
       ];
       $post = json_encode($data);
